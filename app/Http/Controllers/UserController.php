c8d0cf25377c4ce001af403949d295d29e517cc9
NouzNoushad <77887277+NouzNoushad@users.noreply.github.com>
2023-09-20 23:25:04 +0530

    update user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -10,30 +11,47 @@ use Illuminate\Support\Facades\Validator;
 class UserController extends Controller
 {
     public function registerStudent(Request $request){
-        $students = DB::table('users');
+        
         $rules = array(
             'name' => ['required', 'regex: /^[a-zA-Z0-9\s]*$/'],
             'email' => 'required | email | unique:users,email',
-            'password' => 'required | min:6'
+            'password' => ['required', 'min:6', 'confirmed'],
+            'password_confirmation' => ['required'],
         );
-        $validator = Validator::make($request->all(),  $rules);
+        $validator = Validator::make($request->all(), $rules);
         if($validator->fails()){
-            return response()->json($validator->errors(), 402);
+            $data = $validator->errors();
+            $status = 402;
         }else{
-            $students->insertOrIgnore([
+            $data = [
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
                 'created_at' => now(),
                 'updated_at' => now(),
-            ]);
-            if($students){
-                $request->session()->put('email', $request->email);
-                return response()->json(['result' => 'Successfully registered'], 200);
-            }else{
-                return response()->json(['result' => 'Registration failed'], 500);
+            ];
+            DB::beginTransaction();
+            try{
+                $students = DB::table('users');
+                $students->insertOrIgnore($data);
+                DB::commit();
+                
+            }catch (Exception $e){
+                DB::rollBack();
+                p($e->getMessage());
+                $students = null;
             }
+            if($students != null){
+                    $request->session()->put('email', $request->email);
+                    $data = ['result' => 'Successfully registered'];
+                    $status = 200;
+                }else{
+                    $data = ['result' => 'Registration failed'];
+                    $status = 500;
+                    
+                }
         }
+        return response()->json($data, $status);
     }
 
     public function loginStudent(Request $request){
@@ -44,25 +62,32 @@ class UserController extends Controller
         );
         $validator = Validator::make($request->all(),  $rules);
         if($validator->fails()){
-            return response()->json($validator->errors(), 402);
+            $data = $validator->errors();
+            $status = 402;
         }
         else{
             $student = $students->where('email', $request->email)->first();
             if(!$student || !Hash::check($request->password, $student->password)){
-                return response()->json(['result' => 'Invalid user'], 500);
+                $data = ['result' => 'Invalid user'];
+                $status = 500;
             }else{
                 $request->session()->put('email', $request->email);
-                return response()->json(['result' => 'Successfully Login'], 200);
+                $data = ['result' => 'Successfully login'];
+                $status = 200;
             }
         }
+        return response()->json($data, $status);
     }
 
     public function logoutStudent(){
         if(session()->has('email')){
             session()->pull('email');
-            return response()->json(['result' => 'Logout successfully'], 200);
+            $data = ['result' => 'Logout successfully'];
+            $status = 200;
         }else{
-            return response()->json(['result' => 'Logout failed'], 500);
+            $data = ['result' => 'Logout failed'];
+            $status = 500;
         }
+        return response()->json($data, $status);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DemoController;
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,3 +24,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('register_student', [UserController::class, 'registerStudent']);
 Route::post('login_student', [UserController::class, 'loginStudent']);
 Route::post('logout_student', [UserController::class, 'logoutStudent']);
+
+Route::get('/test', function(){
+	p('working');
+});
+
+Route::post('/store', [DemoController::class, 'store']);
+Route::get('/user/{flag}', [DemoController::class, 'index']);
+Route::get('/show/{id}', [DemoController::class, 'show']);
+Route::delete('/delete/{id}', [DemoController::class, 'destroy']);
